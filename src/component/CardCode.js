3306d580cac2c5d69de5d8fcc0ef1953af3c08da
cpar997 <43456531+cpar997@users.noreply.github.com>
2019-11-16 15:34:49 +1300

    added cardCode component

--- src/component/CardCode.js
+++ src/component/CardCode.js
@@ -1,19 +1,37 @@
 import React, { Component } from "react";
+import { get, getCodes } from "../API/CardAPI";
 
 export default class CardCode extends Component {
-  componentDidMount() {
-    this.apiCall();
-  }
-  apiCall = () => {
-    fetch("http://localhost:21337/positional-rectangles?code:something", {
-      method: "GET"
-    })
-      .then(function() {
-        console.log(Response.json());
-      })
-      .catch(function() {});
+  state = { cardList: "" };
+  componentDidMount() {}
+  //   apiCall = () => {
+  //     fetch("http://localhost:21337/positional-rectangles?code:something", {
+  //       method: "GET"
+  //     })
+  //       .then(function() {
+  //         console.log(Response.json());
+  //       })
+  //       .catch(function() {});
+  //   };
+
+  getCodes = () => {
+    return getCodes("bob");
   };
+
+  getCardList = () => {
+    const cardCodes = this.getCodes();
+    const cardList = cardCodes.map(cardCode => get(cardCode.code));
+    return cardList;
+  };
+
   render() {
-    return <div></div>;
+    return this.getCardList().map(card => {
+      return (
+        <>
+          <div>Name:{card.name}</div>
+          <div>Image:{card.image}</div>
+        </>
+      );
+    });
   }
 }
