9fc9a5d8f98c5a1d2bec614babf70cee23bea889
cpar997 <43456531+cpar997@users.noreply.github.com>
2019-11-16 16:35:28 +1300

    added card board

--- src/component/CardCode.js
+++ src/component/CardCode.js
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
 import { get, getCodes } from "../API/CardAPI";
+import {
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography
+} from "@material-ui/core";
+import Darius from "../../src/images/darius-high-noon.jpg";
 
 export default class CardCode extends Component {
   state = { cardList: "" };
@@ -25,13 +35,35 @@ export default class CardCode extends Component {
   };
 
   render() {
-    return this.getCardList().map(card => {
-      return (
-        <>
-          <div>Name:{card.name}</div>
-          <div>Image:{card.image}</div>
-        </>
-      );
-    });
+    return (
+      <Grid container spacing={2} style={{ padding: "20px" }}>
+        {this.getCardList().map(card => {
+          return (
+            <Grid item xs={3}>
+              <Card>
+                <CardActionArea>
+                  <CardMedia
+                    image="../../src/images/darius-high-noon.jpg"
+                    title="Contemplative Reptile"
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {card.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      Josh is cool
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
   }
 }
