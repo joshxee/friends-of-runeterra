27147d2e7b036a784402676fb1d276242fc6e829
cpar997 <43456531+cpar997@users.noreply.github.com>
2019-11-20 22:57:35 +1300

    created userProfile

--- src/component/Session.js
+++ src/component/Session.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import TextField from "@material-ui/core/TextField";
+import { Grid } from "@material-ui/core";
 
 class Session extends Component {
   render() {
@@ -12,24 +13,44 @@ class Session extends Component {
     } = this.props;
 
     return (
-      <div>
-        <TextField
-          id="outlined-basic"
-          label="Session ID"
-          margin="normal"
-          value={sessionId}
-          variant="outlined"
-          onChange={event => onChangeQuery(event.target.value)}
-        />
-        <TextField
-          id="outlined-basic"
-          label="Vorter ID"
-          margin="normal"
-          value={voterId}
-          variant="outlined"
-          onChange={event => onChangeVoter(event.target.value)}
-        />
-      </div>
+      <Grid container>
+        <Grid
+          item
+          xs={6}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            padding: "5px"
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Session ID"
+            margin="normal"
+            value={sessionId}
+            variant="outlined"
+            onChange={event => onChangeQuery(event.target.value)}
+          />
+        </Grid>
+        <Grid
+          item
+          xs={6}
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            padding: "5px"
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Vorter ID"
+            margin="normal"
+            value={voterId}
+            variant="outlined"
+            onChange={event => onChangeVoter(event.target.value)}
+          />
+        </Grid>
+      </Grid>
     );
   }
 }
